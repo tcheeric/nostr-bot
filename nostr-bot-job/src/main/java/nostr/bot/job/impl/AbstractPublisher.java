0732d08ce5e9ad26278d891d7492372a5b81bb1a
tcheeric <tchepane@hey.com>
2023-04-17 23:23:35 +0100

    Improved the sending of messages to the relays

--- nostr-bot-job/src/main/java/nostr/bot/job/impl/AbstractPublisher.java
+++ nostr-bot-job/src/main/java/nostr/bot/job/impl/AbstractPublisher.java
@@ -36,44 +36,44 @@ public class AbstractPublisher implements IPublisher {
     @Override
     public void publish() throws IOException {
 
-        var executor = Executors.newSingleThreadExecutor();
         var jc = new JobConfiguration("data");
         var datafile = jc.getDataFile();
+        var client = new Client("/relays.properties");
 
-        executor.submit(() -> {
+        //Wait it until tried to connect to a half of relays
+        do {
             try {
-
-                var kinds = new KindList();
-                kinds.add(this.kind);
-
-                var referencePubKeys = new PublicKeyList();
-                referencePubKeys.add(this.recipient);
-
-                synchronized (AbstractPublisher.class) {
-                    var since = BotUtil.readLongFromFile(datafile);
-                    
-                    log.log(Level.INFO, "Filtering event since {0}", since);
-                    var filters = Filters.builder().kinds(kinds).referencePubKeys(referencePubKeys).since(since).build();
-
-                    log.log(Level.INFO, "Filters: {0}", filters);
-
-                    //var identity = new Identity("/profile.properties");
-                    var client = new Client("/relays.properties");
-
-                    GenericMessage message = new ReqMessage("nostr-bot", filters);
-
-                    log.log(Level.INFO, "Sending message {0}", message);
-                    client.send(message);
-                }
-
-            } catch (IOException ex) {
-                log.log(Level.SEVERE, null, ex);
-            } finally {
-                synchronized (AbstractPublisher.class) {
-                    var since = Instant.now().getEpochSecond();
-                    BotUtil.storeLongToFile(since, datafile);
-                }
+                log.log(Level.INFO, "Wait it until tried to connect to a half of relays...");
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
             }
-        });
+        } while (client.getThreadPool().getCompletedTaskCount() < (client.getRelays().size() / 2));
+
+        try {
+            var kinds = new KindList();
+            kinds.add(AbstractPublisher.this.kind);
+            var referencePubKeys = new PublicKeyList();
+            referencePubKeys.add(AbstractPublisher.this.recipient);
+
+            synchronized (AbstractPublisher.class) {
+                var since = BotUtil.readLongFromFile(datafile);
+
+                log.log(Level.INFO, "Filtering event since {0}", since);
+                var filters = Filters.builder().kinds(kinds).referencePubKeys(referencePubKeys).since(since).build();
+
+                log.log(Level.INFO, "Filters: {0}", filters);
+
+                GenericMessage message = new ReqMessage("nostr-bot", filters);
+
+                log.log(Level.INFO, "Sending message {0}", message);
+                client.send(message);
+            }
+        } finally {
+            synchronized (AbstractPublisher.class) {
+                var since = Instant.now().getEpochSecond();
+                BotUtil.storeLongToFile(since, datafile);
+            }
+        }
     }
 }
